958c10df6dc69c1a68fb9c159415bf20a398826f
alonso.alejandros <33225275+alesaulo@users.noreply.github.com>
2020-07-23 19:51:11 -0300

    Update and rename HelloController.java to RESTController.java

--- AgileEngineExample/src/main/java/org/AgileEngineExample/RESTController.java
+++ AgileEngineExample/src/main/java/org/AgileEngineExample/RESTController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
-public class HelloController {	
+public class RESTController {	
 	
 	@RequestMapping("/search/{searchTerm}")
 	public String index(@PathVariable("searchTerm") String searchTerm) {
